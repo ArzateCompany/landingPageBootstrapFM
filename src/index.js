a29985b91d41e8b9c6cc58dab7048931d96e96be
Osvaldo Arzate <osvarz.new@gmail.com>
2021-01-11 09:22:48 -0600

    Complete basic webpack config.dev

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './style/test.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const PATH = require('path');
+
+module.exports = {
+  entry: PATH.resolve(__dirname, './src/index.js'),
+  mode: 'development',
+  output: {
+    path: PATH.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
