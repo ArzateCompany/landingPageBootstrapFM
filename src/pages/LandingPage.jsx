6111e4003a288e847a4e0f762f8f677768a41c7d
Osvaldo Arzate <osvarz.new@gmail.com>
2021-01-19 09:55:16 -0600

    complete hero component structure

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Footer from '../components/Footer.jsx';
 import Header from '../components/Header.jsx'
+import Hero from '../components/Hero.jsx';
 
 const LandingPage = () => {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
